2bd45f6d272c65b8f78c650268e878642a8c49e3
Pawan Kumar Siwakoti <pksiwako@deakin.edu.au>
2019-11-08 11:35:14 +1100

    huge world created
    Terrains created along with hiding objects  and fog to make playing area look realistic

---
+++ SuperHero Army/Assets/Scripts/ChunkHider.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunkHider : MonoBehaviour
+{
+    private Terrain[] chunks;
+    public float visibleDistance;
+    public int chunkSize;
+    public float checkRate;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // get all of the chunks 
+        chunks = FindObjectsOfType<Terrain>();
+
+        //Check the chunks every 'checkRate' seconds
+        InvokeRepeating("CheckChunks", 0.0f, checkRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void CheckChunks()
+    {
+        Vector3 playerPos = transform.position;
+        playerPos.y = 0;
+
+        //loop through each chunk 
+        foreach(Terrain chunk in chunks)
+        {
+            //get the center pos of the chunk 
+            Vector3 chunkCenterPos = chunk.transform.position + new Vector3(chunkSize / 2, 0, chunkSize / 2);
+
+            //Check the distance 
+            if (Vector3.Distance(playerPos, chunkCenterPos) > visibleDistance)
+                chunk.gameObject.SetActive(false);
+            else
+                chunk.gameObject.SetActive(true);
+        }
+    }
+}
---
+++ SuperHero Army/Assets/Scripts/ObjectHider.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectHider : MonoBehaviour
+{
+    private Bounds bounds;
+    private GameObject[] hidableObjects;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        // get the hidable objects
+        hidableObjects = GameObject.FindGameObjectsWithTag("ChunkObject");
+
+        //get the bounds of the terrain
+        Terrain terrain = GetComponent<Terrain>();
+
+        Vector3 dimensions = new Vector3(terrain.terrainData.heightmapWidth, 1000, terrain.terrainData.heightmapHeight);
+        Vector3 pos = transform.position + new Vector3(dimensions.x / 2, 0, dimensions.z / 2);
+
+        bounds = new Bounds(pos, dimensions);
+    }
+
+    // Update is called once per frame
+    void OnEnable()
+    {
+        foreach(GameObject obj in hidableObjects)
+        {
+            if (obj != null && bounds.Contains(obj.transform.position))
+                obj.SetActive(true);
+        }
+    }
+    void OnDisable()
+    {
+        foreach(GameObject obj in hidableObjects)
+        {
+            if (obj != null && bounds.Contains(obj.transform.position))
+                obj.SetActive(false);
+        }
+    }
+}
